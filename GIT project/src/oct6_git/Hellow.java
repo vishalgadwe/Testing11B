d07dbf20f84fa24d04c5662027365714dcf20f98
vishalgadwe <91936741+vishalgadwe@users.noreply.github.com>
2021-10-06 12:31:17 +0530

    vishal

--- GIT project/src/oct6_git/Hellow.java
+++ GIT project/src/oct6_git/Hellow.java
@@ -2,6 +2,10 @@ package oct6_git;
 
 public class Hellow {
 
+	public void m1()
+	{
+		System.out.println("m1-----method");
+	}
 	public static void main(String[] args) {
 		
 		System.out.println("hellow");
